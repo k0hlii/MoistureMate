2d747d511b06689558e66f73dd7cc953d2b04bb0
k0hlii <thomas@adiko.at>
2024-10-26 22:35:34 +0200

    Refactor setup code and add SetupWizard class

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,68 @@
 #include <Arduino.h>
+#include <Preferences.h>
+#include <wizard.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Preferences preferences;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(9600);
+  if (!preferences.begin("my-app", false))
+  {
+    Serial.println("Failed to initialize NVS");
+    return;
+  }
+  else
+  {
+    Serial.println("NVS initialized successfully");
+  }
+  
+  // Reinitialize WiFi
+  String ssid = preferences.getString("ssid","");
+  String password = preferences.getString("password","");
+  Serial.println("SSID: " + ssid);
+  Serial.println("Password: " + password);
+
+  if (ssid.length() == 0)
+  {
+    SetupWizard wizard;
+    Serial.println("Setup wizard started");
+    wizard.setup();
+  }
+  else
+  {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid.c_str(), password.c_str());
+    int retries = 0;
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      if (retries > 5)
+      {
+        WiFi.disconnect();
+        Serial.println("Failed to connect to WiFi");
+        SetupWizard wizard;
+        Serial.println("Setup wizard started");
+        wizard.setup();
+        break;
+      }
+      delay(500);
+      Serial.println("Connecting to WiFi...");
+      retries++;
+    }
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  // auto now = millis();
+  // auto delta = now - time1;
+  // if(delta > 1000) {
+  //   Serial.println("10 seconds passed");
+  //   time1 = now;
+  //   Serial.println(WiFi.status());
+  // }
+  String ssid = preferences.getString("ssid","");
+  String password = preferences.getString("password","");
+  Serial.println("SSID: " + ssid);
+  Serial.println("Password: " + password);
 }
